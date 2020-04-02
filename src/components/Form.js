93dfce7cf798bc41d0063380755fda300ba1c87e
Kizito Njoku <nkizito57@gmail.com>
2020-04-02 04:09:31 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -25,6 +25,11 @@ class Form extends React.Component {
     let dataArray = this.state.submittedData.concat(formData)
     this.setState({submittedData: dataArray})
   }
+  
+  listOfSubmissions = () => {
+    return this.state.submittedData.map(data => {
+      return <div><span>{data.firstName}</span> <span>{data.lastName}</span></div>
+    })
 
   render() {
     return (
@@ -32,6 +37,7 @@ class Form extends React.Component {
         <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
         <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
       </form>
+      
     )
   }
 }
