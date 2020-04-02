bd08377dce66bfb0e4ea4c6c9c89962446e7bb75
Kizito Njoku <nkizito57@gmail.com>
2020-04-02 04:09:57 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -37,6 +37,7 @@ class Form extends React.Component {
         <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
         <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
       </form>
+      {this.listOfSubmissions()}
       
     )
   }
